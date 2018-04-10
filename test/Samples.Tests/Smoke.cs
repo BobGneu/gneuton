2a7d5d80d52c6cc75044d0ee5c68e0df7c90e8ba
Bob Chatman <bob.chatman@gmail.com>
2018-04-09 23:11:58 -0700

    Adding test framework project

---
+++ test/Samples.Tests/Smoke.cs
@@ -0,0 +1,14 @@
+﻿namespace Samples.Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class Smoke
+	{
+		[Test]
+		public void ShouldBeEqual()
+		{  
+			Assert.AreEqual(1, 1, 0.01);
+		}
+	}
+}
